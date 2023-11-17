1d997e0da2af603c2e1d120109edc4bb3c17c04e
smartfull178587 <test63012@outlook.com>
2023-11-17 07:10:38 -0500

    update: google_cateogry_id

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 $index = 0;
 
+$curl = curl_init();
+
 while (true) {
+	if ($index == 1) break;
 	$index++;
-	echo $index;
-	sleep(1 * 60 * 60);
 	$store_name = 'littleliffner';
 	$access_token = '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';
 	
-	$curl = curl_init();
 	
 	curl_setopt_array($curl, array(
 		CURLOPT_URL => 'https://'.$store_name.'.myshopify.com/admin/api/2023-10/products.json',
@@ -26,7 +26,6 @@ while (true) {
 	));
 	
 	$response = curl_exec($curl);
-	curl_close($curl);
 	
 	$data = json_decode($response, true);
 	
@@ -36,28 +35,66 @@ while (true) {
 				  'title' . ',' .
 				  'description' . ',' .
 				  'link' . ',' .
+				  'image_link' . ',' .
 				  'availability' . ',' .
 				  'price' . ',' .
+				  'google_product_category' . ',' .
 				  'brand' . ',' .
-				  'gtin' . ',' .
-				  'mpn' . ',' .
+				  'identifier_exists' . ',' .
 				  'condition' . PHP_EOL;
 	
 	foreach ($products as $product) {
+		$description = str_replace(["\r", "\n", "\t"], '', $product['body_html']);
+		$description = str_replace('"', '""', $description);
+		$description = '"' . $description . '"';
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://'.$store_name.'.myshopify.com/admin/api/2020-04/products/' . $product['id'] . '/metafields.json',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'GET',
+			CURLOPT_HTTPHEADER => array(
+				'X-Shopify-Access-Token: '.$access_token
+			),
+		));
+	
+		$response = curl_exec($curl);
+	
+		$result = json_decode($response, true);
+		$product_metafields = $result['metafields'];
+		$condition = 'new';
+		$google_product_category = '';
+		foreach ($product_metafields as $product_metafield) {
+			if ($product_metafield['key'] == 'condition') {
+				$condition = $product_metafield['value'];
+			}
+			if ($product_metafield['key'] == 'google_product_category') {
+				$google_product_category = $product_metafield['value'];
+			}
+		}
+
 		$temp_line = $product['variants'][0]['sku'] . ',' .
 					 $product['title'] . ',' .
-					 'description' . ',' .
-					//  str_replace(["\r", "\n", "\t"], '', $product['body_html']) . ',' .
+					 $description . ',' .
 					 'https://www.littleliffner.com/products/'.$product['handle'] . ',' .
+					 $product['images'][0]['src'] . ',' .
 					 ($product['variants'][0]['inventory_quantity'] == 0 ? 'out of stock' : 'in stock') . ',' .
 					 $product['variants'][0]['price'] . ',' .
-					 '' . ',' .
-					 '' . ',' .
-					 '' . ',' .
-					 'refurbished' . PHP_EOL;
+					 $google_product_category . ',' .
+					 'brand name' . ',' .
+					 'no' . ',' .
+					 $condition . PHP_EOL;
 		$csv_result .= $temp_line;
 	}
 	
 	$file = 'product_feed.csv';
 	file_put_contents($file, $csv_result);
+	echo $csv_result;
+	// sleep(1 * 60 * 60);
 }
+
+curl_close($curl);
